97fe71ebda8e6dd3afeacedb4fcdf4c6022b9ae7
BernardSapida <BernardSapida1706@gmail.com>
2023-03-09 11:42:56 +0800

    first commit

---
+++ Main.java
@@ -0,0 +1,36 @@
+class Main {
+    public static void main(String[] args) {
+        SinglyLinkedList linkedList = new SinglyLinkedList();
+
+        linkedList.push(6);
+        linkedList.push(2);
+        linkedList.push(17);
+        linkedList.push(4);
+        linkedList.push(0);
+
+        linkedList.printList();
+        System.out.println(linkedList.head.value);
+        System.out.println(linkedList.tail.value);
+    }
+}
+
+/*
+ * [1, 2, 3, 4, 5, ...] -> Overflow
+ * 
+ * 
+ * Linked List -> different memory location
+ * 1 -> 2 -> 3 -> 4 -> null
+ * head           tail
+ *                     currentNode
+ * 
+ * Proceedure:
+ * this.tail.next = newNode(2)
+ * this.tail = newNode(2)
+ * 
+ * newNode.value = 1
+ * newNode.next = null
+ * 
+ * visual representation:
+ * 1 -> null
+ * 
+ */
---
+++ Node.java
@@ -0,0 +1,9 @@
+public class Node {
+    int value;
+    Node next; // Pointer
+
+    Node(int value) {
+        this.value = value;
+        this.next = null;
+    }
+}

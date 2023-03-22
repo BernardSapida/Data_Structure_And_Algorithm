80061ea0183c9c41274f9dc7b507f2defd30deef
BernardSapida <BernardSapida1706@gmail.com>
2023-03-22 13:34:00 +0800

    median of two merged array

---
+++ Leetcode/Median of Two Merged Array/main.py
@@ -0,0 +1,44 @@
+def get_median(B, A):
+    total = len(B) + len(A)
+    half = total//2
+
+    if len(B) < len(A):
+        B, A = A, B
+
+    low, high = 0, len(A)-1
+
+    while True:
+        midA = low + (high - low) // 2 # A
+        midB = half - midA - 2 # B
+
+        leftA = A[midA] if midA >= 0 else float("-infinity")
+        rightA = A[midA + 1] if midA + 1 < len(A) else float("infinity")
+        leftB = B[midB] if midB >= 0 else float("-infinity")
+        rightB = B[midB + 1] if midB + 1 < len(B) else float("infinity")
+
+        if leftA <= rightB and leftB <= rightA:
+            if total % 2 == 1:
+                return min(rightA, rightB)
+            else:
+                return (max(leftA, leftB) + min(rightA, rightB)) / 2
+        elif leftA > rightB:
+            high = midA - 1
+        elif leftB > rightA:
+            low = midB + 1
+
+print(get_median([4, 9, 11, 11, 12, 15], [1, 1, 3, 5, 10]))
+
+"""
+    len = 5
+    half = 2
+
+    b = 5 - 2 = 3 - m = 3 - 2 = 1
+    
+     m
+    [1, 3, 5]
+     0   
+    
+     m
+    [2, 4]
+     0
+"""

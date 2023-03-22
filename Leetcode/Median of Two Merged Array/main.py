f130e29934e5eedf391c8894a159081b4a770dd4
BernardSapida <BernardSapida1706@gmail.com>
2023-03-22 13:40:26 +0800

    minimal change

--- Leetcode/Median of Two Merged Array/main.py
+++ Leetcode/Median of Two Merged Array/main.py
@@ -23,7 +23,7 @@ def get_median(B, A):
                 return (max(leftA, leftB) + min(rightA, rightB)) / 2
         elif leftA > rightB:
             high = midA - 1
-        elif leftB > rightA:
+        elif rightA < leftB:
             low = midB + 1
 
 print(get_median([4, 9, 11, 11, 12, 15], [1, 1, 3, 5, 10]))

279b1d47fa7d4b5de808e4d1f67485e4398e3bc9
BernardSapida <BernardSapida1706@gmail.com>
2023-03-26 13:46:29 +0800

    quick sort added

--- Sorting/Quick Sort/main.py
+++ Sorting/Quick Sort/main.py
@@ -1,23 +1,27 @@
 def partition(array, low, high):
-    def swap(arr, i, j):
-        [arr[i], arr[j]] = [arr[j], arr[i]]
+    def swap(array, i, j):
+        [array[i], array[j]] = [array[j], array[i]]
 
-    pivot = array[high]
-    i = low - 1
- 
-    for j in range(low, high):
-        if array[j] <= pivot:
-            i += 1
-            swap(array, i, j)
+    pivot = array[low]
+    pivot_index = low
+
+    for i in range(low+1, high):
+        current_value = array[i]
+        
+        if pivot >= current_value:
+            pivot_index += 1
+            swap(array, pivot_index, i)
     
-    swap(array, i+1, high)
-    return i + 1
+    swap(array, low, pivot_index)
+
+    return pivot_index
  
 def quick_sort(array, low, high):
     if low < high: 
-        pi = partition(array, low, high)
-        quick_sort(array, low, pi - 1)
-        quick_sort(array, pi + 1, high)
+        mid = partition(array, low, high)
+        quick_sort(array, low, mid)
+        quick_sort(array, mid + 1, high)
+
     return array
- 
-print(quick_sort([10, 16, 8, 12, 15, 6, 3, 9, 5], 0, 8))
+
+print(quick_sort([9, 8, 7, 6, 5, 4, 3, 2, 1], 0, 9))

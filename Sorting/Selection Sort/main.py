63b1f5490f131ba7fb282fdbfe18223ed63f7b02
BernardSapida <BernardSapida1706@gmail.com>
2023-03-27 09:48:08 +0800

    selection sort optimized

--- Sorting/Selection Sort/main.py
+++ Sorting/Selection Sort/main.py
@@ -3,15 +3,14 @@ def selection_sort(numbers):
         [numbers[i], numbers[j]] = [numbers[j], numbers[i]]
 
     for i in range(0, len(numbers)):
-        low_index = -1
+        k = i
 
-        for k in range(i+1, len(numbers)):
-            if(numbers[k] < numbers[i]):
-                low_index = k
+        for j in range(i+1, len(numbers)):
+            if(numbers[j] < numbers[i]):
+                k = j
         
-        if low_index == -1:
-            return numbers
-        else:
-            swap(i, low_index)
+        swap(i, k)
+
+    return numbers
 
 print(selection_sort([9, 8, 7, 6, 5, 4, 3, 2, 1]))
